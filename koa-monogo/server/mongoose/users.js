62befa57fd919655a7278fbdfa71f07b5a585448
582004495@qq.com <582004495@qq.com>
2019-04-22 17:28:31 +0800

    mongodb模型修改

--- koa-monogo/server/mongoose/users.js
+++ koa-monogo/server/mongoose/users.js
@@ -1,16 +1,20 @@
 const db = require('./db')
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
     create_time: {
         type: Date,
-        default: Date.now()
+        default: Date.now
     },
     admin: {
         type: String,
         default: 0
+    },
+    status: {
+        type: Number,
+        default: 1
     }
 })
 module.exports = users = db.model('users', userSchema);
