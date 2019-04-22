ac3b01ba93dfdf99f458d363a0355bd0b171e085
582004495@qq.com <582004495@qq.com>
2019-04-22 17:35:58 +0800

    后端index.js  api修改

--- koa-monogo/server/routes/index.js
+++ koa-monogo/server/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')()
 const userApi = require('../api/userApi')
-router.get('/', userApi.Getuser)
+router.get('/getuserList', userApi.Getuser)
 router.post('/loginUser', userApi.loginUser)
     .post('/addUser', userApi.addUser)
 
