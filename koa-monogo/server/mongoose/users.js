21e0c8ea34e279b69791b8584c63e9fba723a5dd
582004495@qq.com <582004495@qq.com>
2019-05-09 17:58:21 +0800

    新增用户模型Schema

--- koa-monogo/server/mongoose/users.js
+++ koa-monogo/server/mongoose/users.js
@@ -1,5 +1,13 @@
 const db = require('./db')
 const mongoose = require('mongoose')
+/**
+ * @type{name}用户名
+ * {email}邮箱
+ * {password}密码
+ * {create_time}创建时间
+ * {admin}管理员权限
+ * {status}发言权限,默认(1)为可发言,0为禁言
+ */
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
